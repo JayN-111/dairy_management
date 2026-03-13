06dc6fcce4262cd4c56c7cafa8bacecf3db69bb1
Jay Nasit <jaynasit111@gmail.com>
2026-03-13 15:32:37 +0530

    --

---
+++ dairy_management/dairy_management/doctype/member_management/member_management.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, jaynasit111@gmail.com and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+
+
+class MemberManagement(Document):
+	pass
---
+++ dairy_management/dairy_management/doctype/milk_collection/milk_collection.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, jaynasit111@gmail.com and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on("Milk Collection", {
+	refresh(frm) {
+
+	},
+});
