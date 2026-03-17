cefc69fae1b8a57b87f93887ce122fdb3c3eb76c
Jay Nasit <jaynasit111@gmail.com>
2026-03-17 12:22:38 +0530

    feat: Implement creation of Purchase Receipts and Invoices from Milk Collection documents

--- dairy_management/dairy_management/doctype/milk_collection/milk_collection.js
+++ dairy_management/dairy_management/doctype/milk_collection/milk_collection.js
@@ -1,39 +1,56 @@
 // Copyright (c) 2026, jaynasit111@gmail.com and contributors
 // For license information, please see license.txt
 
-
-
 frappe.ui.form.on('Milk Collection', {
 
-    // Trigger fetch when fat or milk type changes
-    fat(frm)       { fetch_rate(frm); },
-    milk_type(frm) { fetch_rate(frm); },
-    quantity(frm)  { calculate_price(frm); },
+	refresh(frm) {
+		// Only show Create buttons on submitted documents
+		if (frm.doc.docstatus === 1) {
 
-    // Recalculate total when rate changes
-    rate(frm)      { calculate_price(frm); }
+			frm.add_custom_button(__('Purchase Receipt'), () => {
+				frappe.model.open_mapped_doc({
+					method: 'dairy_management.dairy_management.doctype.milk_collection.milk_collection.make_purchase_receipt',
+					frm: frm
+				});
+			}, __('Create'));
+
+			frm.add_custom_button(__('Purchase Invoice'), () => {
+				frappe.model.open_mapped_doc({
+					method: 'dairy_management.dairy_management.doctype.milk_collection.milk_collection.make_purchase_invoice',
+					frm: frm
+				});
+			}, __('Create'));
+		}
+	},
+
+	// Trigger fetch when fat or milk type changes
+	fat(frm)       { fetch_rate(frm); },
+	milk_type(frm) { fetch_rate(frm); },
+
+	// Recalculate total when quantity or rate changes
+	quantity(frm)  { calculate_price(frm); },
+	rate(frm)      { calculate_price(frm); }
 });
 
 function fetch_rate(frm) {
-    // Only fetch if both fields are filled
-    if (!frm.doc.milk_type || !frm.doc.fat) return;
+	if (!frm.doc.milk_type || !frm.doc.fat) return;
 
-    frappe.call({
-        method: 'dairy_management.dairy_management.doctype.milk_collection.milk_collection.get_milk_rate',
-        args: {
-            milk_type: frm.doc.milk_type,
-            fat: frm.doc.fat
-        },
-        callback(r) {
-            if (r.message) {
-                frm.set_value('rate', r.message);
-                calculate_price(frm);  // recalculate total immediately
-            }
-        }
-    });
+	frappe.call({
+		method: 'dairy_management.dairy_management.doctype.milk_collection.milk_collection.get_milk_rate',
+		args: {
+			milk_type: frm.doc.milk_type,
+			fat: frm.doc.fat
+		},
+		callback(r) {
+			if (r.message) {
+				frm.set_value('rate', r.message);
+				calculate_price(frm);
+			}
+		}
+	});
 }
 
 function calculate_price(frm) {
-    let price = (frm.doc.quantity || 0) * (frm.doc.fat || 0) * (frm.doc.rate || 0);
-    frm.set_value('total_price', price);
+	let price = (frm.doc.quantity || 0) * (frm.doc.fat || 0) * (frm.doc.rate || 0);
+	frm.set_value('total_price', price);
 }
